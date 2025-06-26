3e699ed32f4b9edd30d00de0d3ee35d90723f3c3
ohey <huynhgiabao853@gmail.com>
2025-06-26 17:26:09 +0700

    update-login-admin

--- src/routes/Admin.route.ts
+++ src/routes/Admin.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerAdmin, verifyAdmin, loginAdmin, logoutAdmin, getAdminProfile, verifyLogin } from '../controllers/Admin.controller';
+import { registerAdmin, verifyAdmin, loginAdmin, logoutAdmin, getAdminProfile } from '../controllers/Admin.controller';
 import { isAuthenticated } from '../middlewares/auth/auth.middleware';
 
 const adminRouter = express.Router();
@@ -7,8 +7,6 @@ const adminRouter = express.Router();
 adminRouter.post('/register', registerAdmin);
 adminRouter.post('/verify', verifyAdmin);
 adminRouter.post('/login', loginAdmin);
-adminRouter.post('/login/verify', verifyLogin);
-
 adminRouter.post('/logout', isAuthenticated, logoutAdmin);
 adminRouter.get('/profile', isAuthenticated, getAdminProfile);
 
