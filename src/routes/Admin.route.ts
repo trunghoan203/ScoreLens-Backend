eb5a340b90164594d7efb84ff0e55d02d56b887a
ohey <huynhgiabao853@gmail.com>
2025-06-27 10:08:50 +0700

    admin-resetPassword

--- src/routes/Admin.route.ts
+++ src/routes/Admin.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerAdmin, verifyAdmin, loginAdmin, logoutAdmin, getAdminProfile } from '../controllers/Admin.controller';
+import { registerAdmin, verifyAdmin, loginAdmin, logoutAdmin, getAdminProfile, forgotPassword, verifyResetCode, setNewPassword } from '../controllers/Admin.controller';
 import { isAuthenticated } from '../middlewares/auth/auth.middleware';
 
 const adminRouter = express.Router();
@@ -7,6 +7,9 @@ const adminRouter = express.Router();
 adminRouter.post('/register', registerAdmin);
 adminRouter.post('/verify', verifyAdmin);
 adminRouter.post('/login', loginAdmin);
+adminRouter.post('/forgotPassword', forgotPassword);
+adminRouter.post('/verify-resetCode', verifyResetCode);
+adminRouter.post('/set-newPassword', setNewPassword);
 adminRouter.post('/logout', isAuthenticated, logoutAdmin);
 adminRouter.get('/profile', isAuthenticated, getAdminProfile);
 
